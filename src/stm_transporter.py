61d1450be0ec04a180e26c31549c66a3408814b2
Romster101 <115043070+Romster101@users.noreply.github.com>
2024-03-04 19:44:10 +0300

    Update stm_transporter.py

--- src/stm_transporter.py
+++ src/stm_transporter.py
@@ -5,14 +5,14 @@ import roslib
 import serial 
 from std_msgs.msg import String
 
-# port = "ИМЯ ПОРТА СТМКИ С ДВИГАТЕЛЕМ"
-# mySerialPort = serial.Serial(port)
+port = "ttyACM0"
+mySerialPort = serial.Serial(port)
 
 def callback(data):
     print(data)
-     # mySerialPort.write(data.data.encode())
+    mySerialPort.write(data.data.encode())
 
 rospy.init_node("stm_transporter")
 rospy.Subscriber("transporter_topic", String, callback)
 rospy.spin()
-# mySerialPort.close()
+mySerialPort.close()
